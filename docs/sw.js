b6eb69ee4747dab0e18a768149a76c429858dcf1
cadengl-oss <cadengl@hotmail.com>
2026-09-17 02:40:56 -0700

    feat: add Forge Wall Remote to iPad command deck

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pse-command-deck-v1";
+const CACHE = "pse-command-deck-v2";
 const ASSETS = ["./","./styles.css","./app.js","./manifest.webmanifest","./icon-180.png","./icon-512.png"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
